570c7b02626de4848ee8cfb14078b0cae40c41be
Kanisorn Sutham <firstziiz.k@gmail.com>
2021-01-22 11:34:24 +0700

    feat(mousetail): disable mousetail

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 import cx from "classnames"
 
-import Canvas from "../MouseTrail"
 import Header from "../Header"
 import "./Layout.scss"
 
 const Layout: React.FC = ({ children }) => (
   <React.Fragment>
-    <Canvas />
     <div className={cx("container mx-auto")}>
       <Header />
       <div>{children}</div>
