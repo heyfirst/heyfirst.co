d66379e1ed6ea793fd641f40c536849559512e78
Kanisorn Sutham <firstziiz.k@gmail.com>
2021-01-24 09:31:46 +0700

    fix(gtag): fix gtag

--- src/html.tsx
+++ src/html.tsx
@@ -37,7 +37,6 @@ const HTML: React.FC = (props: any) => (
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-3TZ47KP0Q7');`,
         }}
       />
