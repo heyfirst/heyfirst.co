99b6a89d058eff312b56aa6ad8e4edb90e38070d
Kanisorn Sutham <kanisorns.k@gmail.com>
2021-04-21 00:01:10 +0700

    feat(tailwind): add typography tailwind plugin

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="text-3xl">
+      <main className="prose">
+        <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
+const { spacing, fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-opaque": "rgb(13 42 148 / 18%)",
+      },
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.700"),
+            a: {
+              color: theme("colors.blue.500"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+              },
+              code: { color: theme("colors.blue.400") },
+            },
+            "h2,h3,h4": {
+              "scroll-margin-top": spacing[32],
+            },
+            code: { color: theme("colors.pink.500") },
+            "blockquote p:first-of-type::before": false,
+            "blockquote p:last-of-type::after": false,
+          },
+        },
+      }),
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
