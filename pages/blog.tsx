33b72a486f54a4d8c63f31cbb8cc244a9a688335
Kanisorn Sutham <kanisorns.k@gmail.com>
2021-04-26 23:54:14 +0700

    feat(preact): add preact to the project

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Container from "@/components/Container";
 import BlogPost from "@/components/BlogPost";
 import { getAllFilesFrontMatter } from "@/lib/mdx";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Container from "@/components/Container";
 
 export default function Home() {
