a1b786a69f9fe4d045d15304bb6ca1ac21239f21
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-05-01 01:04:28 +0300

    add migration deploy when run app from docker image

---
+++ apps/heyfirst-api/docker/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Apply database migrations
+echo "### Apply database migrations ###"
+npx prisma migrate deploy --schema "./apps/heyfirst-api/prisma/schema.prisma"
+
+# Start app
+echo "### Starting app ###"
+node ./apps/heyfirst-api/dist/index.js
