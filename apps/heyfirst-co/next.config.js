61435fa7b725dc6c7b92bc9bd6e96eea6af44096
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-05-05 16:32:21 +0300

    update content security policy

--- apps/heyfirst-co/next.config.js
+++ apps/heyfirst-co/next.config.js
@@ -50,7 +50,7 @@ module.exports = {
 // https://securityheaders.com
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com heyfirst.ck.page *.convertkit.com *.googletagmanager.com *.spotify.com giscus.app cdnjs.cloudflare.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com heyfirst.ck.page *.convertkit.com *.googletagmanager.com *.spotify.com giscus.app cdnjs.cloudflare.com *.heyfirst.co;
   child-src *.youtube.com *.google.com *.twitter.com heyfirst.ck.page *.convertkit.com *.spotify.com giscus.app giphy.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com giscus.app;
   img-src * blob: data:;
