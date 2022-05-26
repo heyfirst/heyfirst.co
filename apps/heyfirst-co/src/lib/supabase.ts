39067ed08e1572aa82bd4a67ea79d5297064ad77
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-05-26 20:27:54 +0300

    remove supabase

--- apps/heyfirst-co/src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export default supabase;
