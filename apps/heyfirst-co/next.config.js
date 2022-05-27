76ab6e26e50451e373106bbe7b44d2911dfe8314
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-05-27 16:08:44 +0300

    fix: unable preact for now

--- apps/heyfirst-co/next.config.js
+++ apps/heyfirst-co/next.config.js
@@ -11,20 +11,21 @@ module.exports = {
       },
     ];
   },
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config, { isServer }) => {
     if (isServer) {
       require("./scripts/generate-sitemap");
       require("./scripts/generate-rss");
     }
 
+    // TODO: uncomment once Preact stable for react18
     // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
+    // if (!dev && !isServer) {
+    //   Object.assign(config.resolve.alias, {
+    //     react: "preact/compat",
+    //     "react-dom/test-utils": "preact/test-utils",
+    //     "react-dom": "preact/compat",
+    //   });
+    // }
 
     return config;
   },
