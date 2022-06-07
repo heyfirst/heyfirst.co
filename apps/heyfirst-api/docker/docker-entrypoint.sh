b2ebee1ea5df95a814eb75a14f08780c6fe74b74
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-06-07 22:11:22 +0300

    chore: update docker image

--- apps/heyfirst-api/docker/docker-entrypoint.sh
+++ apps/heyfirst-api/docker/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e
 
-# Apply database migrations
-echo "### Apply database migrations ###"
-npx prisma migrate deploy --schema "./apps/heyfirst-api/prisma/schema.prisma"
-
 # Start app
 echo "### Starting app ###"
 node ./apps/heyfirst-api/dist/index.js
