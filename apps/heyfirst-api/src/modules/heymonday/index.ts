92c05957bf6435cfd45a265a1b01274634857572
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-06-09 00:18:45 +0300

    feat: add chat id protection

--- apps/heyfirst-api/src/modules/heymonday/index.ts
+++ apps/heyfirst-api/src/modules/heymonday/index.ts
@@ -1,42 +1,52 @@
 import type { FastifyPluginCallback, RouteShorthandMethod } from "fastify";
 
+type TelegramRequestBody = {
+  update_id: number;
+  message_id: number;
+  message: {
+    date: number;
+    chat: {
+      id: number;
+      first_name: string;
+      last_name: string;
+      username: string;
+      type: "private";
+    };
+    from: {
+      id: number;
+      username: string;
+      first_name: string;
+      last_name: string;
+      language_code: string;
+      is_bot: boolean;
+    };
+    text: string;
+  };
+};
+
 type TelegramWebhookHandler = RouteShorthandMethod & {
   Params: { secretToken: string };
-  Body: {
-    update_id: number;
-    message: {
-      date: number;
-      chat: {
-        last_name: string;
-        id: number;
-        first_name: string;
-        username: string;
-      };
-      message_id: 1365;
-      from: {
-        last_name: string;
-        id: number;
-        first_name: string;
-        username: string;
-      };
-      text: string;
-    };
-  };
+  Body: TelegramRequestBody;
 };
 
 const TELEGRAM_ROUTE_SECRET_TOKEN = process.env.TELEGRAM_ROUTE_SECRET_TOKEN;
 const TELEGRAM_SECRET_TOKEN = process.env.TELEGRAM_SECRET_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 const base: FastifyPluginCallback = (app, _, done) => {
   app.post<TelegramWebhookHandler>(
     "/webhook/:secretToken",
-    async (req, reply) => {
-      if (req.params.secretToken !== TELEGRAM_ROUTE_SECRET_TOKEN) {
+    async ({ body, params, log }, reply) => {
+      const isChatID = body.message.chat.id.toString() === TELEGRAM_CHAT_ID;
+      const isSecretRoute = params.secretToken === TELEGRAM_ROUTE_SECRET_TOKEN;
+
+      if (!isChatID || !isSecretRoute) {
         reply.status(403);
         return reply.send();
       }
 
-      req.log.info(req.body);
+      // TODO(telegram): reply back to correct chat id with Yah!
+      log.info({ message: "Yah!" });
 
       reply.status(200);
       return reply.send();
