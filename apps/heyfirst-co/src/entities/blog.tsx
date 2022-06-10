ee552db5f13a23adbfb6994dabdf41b5c3923d07
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-06-10 14:32:49 +0300

    refactor: blog page view via react-query

---
+++ apps/heyfirst-co/src/entities/blog.tsx
@@ -0,0 +1,29 @@
+import reactQueryClient from "@/lib/react-query";
+import { useMutation, useQuery } from "react-query";
+import numberWithCommas from "src/lib/numberWithCommas";
+
+const blogPageViewQueryId = (slug: string) => ["blog", "page_view", slug];
+
+export const useBlogPageView = (slug: string) => {
+  const fetcher = () =>
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog/page_views/${slug}`).then(
+      (res) => res.json()
+    );
+
+  return useQuery(blogPageViewQueryId(slug), fetcher, {
+    select: (data) => numberWithCommas(data?.totalCount),
+  });
+};
+
+export const useUpdateBlogPageView = (slug: string) => {
+  const fetcher = () =>
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/blog/page_views/${slug}`, {
+      method: "POST",
+    }).then((res) => res.json());
+
+  return useMutation(fetcher, {
+    onSuccess: (data) => {
+      reactQueryClient.setQueryData(blogPageViewQueryId(slug), data);
+    },
+  });
+};
