1d482dac1a4394db1c1eb585374b15dc0377ab7e
Kanisorn Sutham <kanisorns.k@gmail.com>
2022-06-10 14:40:30 +0300

    fix: add staleTime

--- apps/heyfirst-co/src/lib/react-query.ts
+++ apps/heyfirst-co/src/lib/react-query.ts
@@ -1,5 +1,11 @@
 import { QueryClient } from "react-query";
 
-const reactQueryClient = new QueryClient();
+const reactQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
 
 export default reactQueryClient;
