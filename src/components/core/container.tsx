35b51b29b681942286325194b12d47d95c681e41
First Kanisorn Sutham <kanisorn.sutham@unity3d.com>
2022-10-08 13:14:31 +0300

    feat: add next-seo

--- src/components/core/container.tsx
+++ src/components/core/container.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from "next-seo";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
@@ -70,26 +71,39 @@ const Container: React.FC<React.PropsWithChildren<Meta>> = ({
   return (
     <div className="">
       <Head>
-        <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
-        <meta name="keywords" content={meta.tags.join(", ")} />
-        <meta property="og:url" content={meta.url} />
-        <link rel="canonical" href={meta.url} />
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content={meta.name} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={meta.twitter_id} />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-
-        {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
-        )}
+        <NextSeo
+          title={meta.title}
+          description={meta.description}
+          canonical={meta.url}
+          openGraph={{
+            url: meta.url,
+            title: meta.title,
+            description: meta.description,
+            images: [
+              {
+                url: meta.image,
+                alt: `meta.title Alt`,
+              },
+            ],
+            site_name: meta.name,
+            type: "article",
+            article: {
+              publishedTime: meta.date,
+              tags: meta.tags,
+            },
+          }}
+          twitter={{
+            handle: meta.twitter_id,
+            site: meta.twitter_id,
+            cardType: "summary_large_image",
+          }}
+          additionalMetaTags={[
+            {
+              property: "keywords",
+              content: meta.tags.join(", ") || "",
+            },
+          ]}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
