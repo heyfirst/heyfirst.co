3a405a6f10f903895830d60423e4dc1a6be6d2df
First Kanisorn Sutham <kanisorn.sutham@unity3d.com>
2022-10-08 17:57:14 +0300

    fix: revert @types/react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react";
+import Head from "next/head";
 import Providers from "@/components/providers";
 import { AppProps } from "next/app";
 
@@ -7,6 +8,14 @@ import "@/styles/index.css";
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
     <Providers>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+        <link
+          href="https://fonts.cdnfonts.com/css/jetbrains-mono?styles=60145,60142"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
     </Providers>
   );
